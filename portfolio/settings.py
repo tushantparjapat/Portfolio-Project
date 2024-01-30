e93ff46a7ec12611cad3cdc894af9555c0751730
tushantparjapat <79953243+tushantparjapat@users.noreply.github.com>
2024-01-30 18:47:54 +0530

    Update settings.py
    remove smtp

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,12 +25,7 @@ print(BASE_DIR)
 # email send
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dealleadonline@gmail.com'
-EMAIL_HOST_PASSWORD = 'nrapppngvwextecy'
-EMAIL_USE_TLS = True  # or False if you're using SSL
-DEFAULT_FROM_EMAIL = 'dealleadonline@gmail.com'
+
 
 # def send_mail( email, message):
 #     subject = "OTP for reset password "
